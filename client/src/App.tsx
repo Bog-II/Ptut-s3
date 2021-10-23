2e2d45f39502c44b65839897c0231dc0970f8673
teleyos <tristanduret@hotmail.fr>
2021-10-24 00:26:04 +0200

    react router implementation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,46 +1,51 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Authentification from "./pages/Authentification";
+import Docs from "./pages/Docs";
+import Home from "./pages/Home";
+import Registration from "./pages/Registration";
 
-/**
- * Application component
- * @return {component}
- */
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/authentification">Authentification</Link>
+            </li>
+            <li>
+              <Link to="/registration">Registration</Link>
+            </li>
+            <li>
+              <Link to="/docs">Editer Document</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/docs">
+            <Docs />
+          </Route>
+          <Route path="/registration">
+            <Registration />
+          </Route>
+          <Route path="/authentification">
+            <Authentification />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ client/src/pages/Docs.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Editor from '../components/Editor'
+
+const Docs = () => {
+	return (
+		<div>
+			<h1>Docs</h1>
+			<Editor></Editor>
+		</div>
+	)
+}
+
+export default Docs
