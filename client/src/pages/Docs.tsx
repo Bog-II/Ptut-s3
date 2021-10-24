1acdc1c4c3fb0d3395a19d0fb8ae8174c012c59a
teleyos <tristanduret@hotmail.fr>
2021-10-24 19:10:04 +0200

    changement style Docs

--- client/src/pages/Docs.tsx
+++ client/src/pages/Docs.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Editor from '../components/Editor'
+import "./Docs.css"
+import Logo from "../../resources/logo.png"
 
 const Docs = () => {
+
+	let document = {
+		title: "Titre du document",
+		text: "",
+	}
+
 	return (
 		<div>
-			<h1>Titre du document</h1>
-			<Editor></Editor>
+			<div className="editor-header">
+				<img className="logo" src={Logo} />
+				<h1 className="document-title">{document.title}</h1>
+			</div>
+			<Editor text={document.text} ></Editor>
 		</div>
 	)
 }
