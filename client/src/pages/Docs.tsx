67f1c95f04757999c7969ab16cbc476ce3a8d7ee
teleyos <tristanduret@hotmail.fr>
2021-10-25 12:12:56 +0200

    compatibility with mongo

--- client/src/pages/Docs.tsx
+++ client/src/pages/Docs.tsx
@@ -1,24 +1,18 @@
-import React from 'react'
-import Editor from '../components/Editor'
-import Logo from '../components/Logo'
-import "./Docs.css"
+import React from 'react';
+import Editor from '../components/Editor';
+import Logo from '../components/Logo';
+import './Docs.css';
 
 const Docs = () => {
+  return (
+    <div>
+      <div className="editor-header">
+        <Logo />
+        <h1 className="document-title">{document.title}</h1>
+      </div>
+      <Editor></Editor>
+    </div>
+  );
+};
 
-	let document = {
-		title: "Titre du document",
-		text: "",
-	}
-
-	return (
-		<div>
-			<div className="editor-header">
-				<Logo />
-				<h1 className="document-title">{document.title}</h1>
-			</div>
-			<Editor text={document.text} ></Editor>
-		</div>
-	)
-}
-
-export default Docs
+export default Docs;
