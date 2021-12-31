381464c27a4e92f7b3d653828093611081c446d9
Bogdan <bogsacha@gmail.com>
2021-12-31 14:44:42 +0100

    Docs Compoenent modifications

--- client/src/pages/Docs.tsx
+++ client/src/pages/Docs.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import Editor from '../components/Editor';
+import Header from '../components/Header';
 import Logo from '../components/Logo';
+import ProfilePicture from '../components/ProfilePicture';
 import './Docs.css';
 
 const Docs = () => {
   return (
     <div>
-      <div className="editor-header">
-        <Logo />
-        <h1 className="document-title">{document.title}</h1>
-      </div>
-      <Editor></Editor>
+      <Header />
+      <Editor />
     </div>
   );
 };
