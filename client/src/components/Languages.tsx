a959dc67c5403b3a9271b400188cbd7fe57a5763
Bogdan <bogsacha@gmail.com>
2022-01-09 21:05:52 +0100

    languages

---
+++ client/src/components/Languages.tsx
@@ -0,0 +1,15 @@
+import Logo from './Logo';
+import ProfilePictures from './ProfilePictures';
+import './Header.css';
+import ShareButton from './ShareButton';
+
+const Languages = () => {
+  return (
+    <div className="languages-container">
+      <span className="language-span">FR</span>|
+      <span className="language-span">EN</span>
+    </div>
+  );
+};
+
+export default Languages;
