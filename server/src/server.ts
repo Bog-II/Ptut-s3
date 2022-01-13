532e44416eb332d611be26a0e59c0cee98c974a3
Bogdan <bogsacha@gmail.com>
2022-01-14 00:05:30 +0100

    put

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,6 +15,7 @@ const io = require('socket.io')(process.env.SOCKET_PORT, {
     // ],
     origin: '*',
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   },
 });
 
