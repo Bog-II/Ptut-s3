9114270745b7466b98c8b394abf5ff8f59b46edc
Bogdan <bogsacha@gmail.com>
2022-03-27 14:10:00 +0200

    registration form build

--- client/src/components/forms/RegistrationForm/RegistrationForm.tsx
+++ client/src/components/forms/RegistrationForm/RegistrationForm.tsx
@@ -1,7 +1,113 @@
-import React from 'react'
+import { Clear, Visibility, VisibilityOff } from '@mui/icons-material'
+import { IconButton, InputAdornment, TextField } from '@mui/material'
+import React, { useState } from 'react'
+import { useTranslation } from 'react-i18next';
 
 export const RegistrationForm = () => {
+  const { t, i18n } = useTranslation('forms');
+
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [passwordValue, setPasswordValue] = useState<string>('');
+
+  const handleIconPasswordClick = () => {
+    setShowPassword(!showPassword);
+  }
+
   return (
-    <div>RegistrationForm</div>
+    <>
+      <h1>{t("signUp")}</h1>
+
+      <TextField
+        id="standard-basic"
+        label={t("userName")}
+        color="primary"
+        type="text"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              {/* {documentContext.searchBarValue != '' ? (
+                <IconButton onClick={onValueClear}>
+                  <Clear />
+                </IconButton>
+              ) : null} */}
+              <IconButton>
+                <Clear />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+
+      <TextField
+        id="standard-basic"
+        label={t("emailAdress")}
+        color="primary"
+        type="email"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              {/* {documentContext.searchBarValue != '' ? (
+                <IconButton onClick={onValueClear}>
+                  <Clear />
+                </IconButton>
+              ) : null} */}
+              <IconButton>
+                <Clear />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+
+      <TextField
+        id="standard-basic"
+        label={t("password")}
+        color="primary"
+        type={showPassword ? "text" : "password"}
+        autoComplete="on"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              {/* {documentContext.searchBarValue != '' ? (
+                <IconButton onClick={onValueClear}>
+                  <Clear />
+                </IconButton>
+              ) : null} */}
+              <IconButton>
+                <Clear />
+              </IconButton>
+              <IconButton onClick={handleIconPasswordClick}>
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+
+      <TextField
+        id="standard-basic"
+        label={t("confirmPassword")}
+        color="primary"
+        type={showPassword ? "text" : "password"}
+        autoComplete="on"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              {/* {documentContext.searchBarValue != '' ? (
+                <IconButton onClick={onValueClear}>
+                  <Clear />
+                </IconButton>
+              ) : null} */}
+              <IconButton>
+                <Clear />
+              </IconButton>
+              <IconButton onClick={handleIconPasswordClick}>
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+    </>
   )
 }
