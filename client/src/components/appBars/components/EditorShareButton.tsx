8d114a7e7ce4742d2915cb216ba94edd9b711999
Bogdan <bogsacha@gmail.com>
2022-03-28 00:57:17 +0200

    editor share document dialog

--- client/src/components/appBars/components/EditorShareButton.tsx
+++ client/src/components/appBars/components/EditorShareButton.tsx
@@ -1,13 +1,95 @@
-import { Link } from '@mui/icons-material'
-import { Button } from '@mui/material'
+import { ContentPaste, Link } from '@mui/icons-material'
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogActions,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Tooltip
+} from '@mui/material'
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { copyToClipboard } from '../../../utils/Editor';
 
 export const EditorShareButton = () => {
   const { t, i18n } = useTranslation();
 
+  const [shareDialogoOpen, setShareDialogoOpen] = useState<boolean>(false);
+  const [shareEditorId, setShareEditorId] = useState<string>('');
+  const [shareLectureId, setShareLectureId] = useState<string>('');
+
+  const showShareDialog = (documentId: string) => {
+    console.log(documentId);
+    setShareEditorId('linkEditor');
+    setShareLectureId('linkLecture');
+    setShareDialogoOpen(true);
+  }
+
+  const hideShareDialog = () => {
+    setShareDialogoOpen(false);
+  }
+
   return (
-    <Button variant="contained" startIcon={<Link />}>
-      {t('share')}
-    </Button>
+    <>
+      <Button variant="contained" onClick={() => showShareDialog("documentId")} startIcon={<Link />}>
+        {t('share')}
+      </Button>
+
+      <Dialog
+        open={shareDialogoOpen}
+        onClose={hideShareDialog}
+        fullWidth>
+        <DialogTitle>{t('shareDocument')}</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            value={shareEditorId}
+            margin="dense"
+            label={t('editor')}
+            fullWidth
+            variant="outlined"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  {shareEditorId != '' ? (
+                    <Tooltip title={t('copy').toString()}>
+                      <IconButton onClick={() => copyToClipboard(shareEditorId)}>
+                        <ContentPaste />
+                      </IconButton>
+                    </Tooltip>
+                  ) : null}
+                </InputAdornment>
+              ),
+            }} />
+
+          <TextField
+            autoFocus
+            value={shareLectureId}
+            margin="dense"
+            label={t('lecture')}
+            fullWidth
+            variant="outlined"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  {shareLectureId != '' ? (
+                    <Tooltip title={t('copy').toString()}>
+                      <IconButton onClick={() => copyToClipboard(shareLectureId)}>
+                        <ContentPaste />
+                      </IconButton>
+                    </Tooltip>
+                  ) : null}
+                </InputAdornment>
+              ),
+            }} />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={hideShareDialog}>{t('hide')}</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   )
 }
